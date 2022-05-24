b81b992d84b2b85755a6929c0955975ea3756290
nehuenignaciog <nehuenignaciog@gmail.com>
2022-05-23 22:05:42 -0300

    Pendiente revision

--- ejercicios_practica/ejercicio1_.py
+++ ejercicios_practica/ejercicio1_.py
@@ -26,11 +26,14 @@ def serializar():
     # Que su lista de prendas dentro del JSON tenga al menos 2 prendas
 
     # json_data = {...}
-
+    json_data = {'Nobre': 'Nehuen', 'Apellido': 'Gonzalez'  , 'DNI':'33991534','vestimenta':[{'prenda':'Zapatilla','cantida':4},{'prenda':'buzo','cantida':1}]}
     # Una vez que finalice el JSON realice un "dump" para almacenarlo en
     # un archivo que usted defina
-
+    print (json_data)
     # Observe el archivo y verifique que se almaceno lo deseado
+    with open ('json_nig','w') as jsonfile:
+        json.dump(json_data,jsonfile, indent=4)
+        print (json.dump)
 
 
 def deserializar():
@@ -39,6 +42,15 @@ def deserializar():
     # Basado en la función  anterior debe abrir y leer el contenido
     # del archivo y guardarlo en un objeto JSON utilizando el método
     # load()
+    with open ('json_nig','r') as jsonfile:
+        jason_nig = json.load(jsonfile)
+    
+    print (jason_nig)
+
+    
+    print ( json.dumps(jason_nig,indent = 4))
+
+
 
     # Luego debe convertir ese JSON data en json_string utilizando
     # el método "dumps" y finalmente imprimir en pantalla el resultado
